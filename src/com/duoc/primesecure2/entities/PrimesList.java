d402bf51e3d1091cc7a1055cbcd4243f48d9df8d
LeoOlivaresD <cspraword@gmail.com>
2024-07-05 01:38:19 -0400

    refactor

--- src/com/duoc/primesecure2/entities/PrimesList.java
+++ src/com/duoc/primesecure2/entities/PrimesList.java
@@ -23,8 +23,8 @@ public class PrimesList extends ArrayList<Integer> {
     public void setModCount(int modCount) {
         this.modCount = modCount;
     }
-    
-    //Sobreescribo los metodos "add" y remove"
+
+    // Sobreescribo los metodos "add" y "remove"
     @Override
     public boolean add(Integer numero) {
         if (!isPrime(numero)) {
@@ -32,7 +32,7 @@ public class PrimesList extends ArrayList<Integer> {
         }
         return super.add(numero);
     }
-    //Con este metodo eliminare el  primer objeto de la lista especificado
+
     @Override
     public boolean remove(Object o) {
         if (o instanceof Integer) {
@@ -40,39 +40,34 @@ public class PrimesList extends ArrayList<Integer> {
         }
         return super.remove(o);
     }
-    //Aca eliminare un elemento segun el indice indicado
+
     @Override
     public Integer remove(int index) {
         Integer numero = get(index);
         System.out.println("Eliminando el número " + numero + " de la lista.");
         return super.remove(index);
     }
-    
-    //Con este metodo determino si el numero es primo
+
+    // Con este metodo determino si el numero es primo
     public static boolean isPrime(Integer numero) {
-        //Verifico primero si el numero es igual o menor a 1 para descartar de inmediato ese valor 
         if (numero <= 1) {
-            System.out.println("Numeros menores o igual 1 no son primoss");
+            System.out.println("Numeros menores o igual 1 no son primos");
             return false;  // 1 no es primo
         }
-        //Aca verifico si el numero es divisible por algun numero entero entre 2 y su raiz cuadrada
         for (int i = 2; i <= Math.sqrt(numero); i++) {
             if (numero % i == 0) {
                 System.out.println("No es primo");
                 return false;
             }
         }
-        //Si no encuentro un divisor , el numero es primo
-        System.out.println("Es primo");
         return true;
-
     }
-    //Con este metodo imprimo una lista de numeros primos
-    public void getPrimesCount(PrimesList primesList) {
+
+    // Con este metodo imprimo una lista de numeros primos
+    public void getPrimesCount() {
         System.out.println("Imprimiendo numeros primos de la lista");
-        for (int n : primesList) {
+        for (int n : this) { //con este this me refiero a la clase misma y como es una lista la recorre con un for each
             System.out.println(n);
         }
     }
-    
 }
