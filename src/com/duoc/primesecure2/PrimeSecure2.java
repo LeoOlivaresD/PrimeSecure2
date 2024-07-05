a7350905cc1edc8cf5ca6a6c03fcdaa66d60e2e7
LeoOlivaresD <cspraword@gmail.com>
2024-07-05 03:12:21 -0400

    Refactor

--- src/com/duoc/primesecure2/PrimeSecure2.java
+++ src/com/duoc/primesecure2/PrimeSecure2.java
@@ -1,14 +1,18 @@
 package com.duoc.primesecure2;
 
+import com.duoc.primesecure2.entities.Mensajeria;
 import com.duoc.primesecure2.entities.PrimesList;
 import com.duoc.primesecure2.input.ReaderPrimes;
+import com.duoc.primesecure2.output.MensajeEncriptado;
 import com.duoc.primesecure2.output.WriterPrimesCsv;
 import com.duoc.primesecure2.threads.PrimesThread;
+import java.util.Scanner;
 
 public class PrimeSecure2 {
 
     public static void main(String[] args) {
         // Creación de objetos
+        Mensajeria mensajeria = new Mensajeria();
         PrimesList primesList = new PrimesList();
         PrimesThread runnable = new PrimesThread(primesList);//paso el objeto de primesList en el constructor de mi clase PrimesThread
         Thread thread = new Thread(runnable);
@@ -24,5 +28,13 @@ public class PrimeSecure2 {
         System.out.println("Generando archivo csv");
         WriterPrimesCsv.savePrimesListToCsv(primesList);
         ReaderPrimes.readerCsv();
+        
+        System.out.println("Ingrese un mensaje");
+        Scanner sc = new Scanner(System.in);
+        String mensaje = sc.nextLine();
+        MensajeEncriptado.generarMensaje(mensajeria, primesList.get(0), mensaje);
+        
+        System.out.println(mensajeria.toString());
+        
     }
 }
