c0b0ae144380bab15715c61c4cb15659e79d3c21
LeoOlivaresD <cspraword@gmail.com>
2024-07-05 01:35:09 -0400

    Se desarrolla logica de hilo para generar y agregar numeros primos a una lista

--- src/com/duoc/primesecure2/threads/PrimesThread.java
+++ src/com/duoc/primesecure2/threads/PrimesThread.java
@@ -1,10 +1,37 @@
 package com.duoc.primesecure2.threads;
 
-public class PrimesThread implements Runnable{
+import com.duoc.primesecure2.entities.PrimesList;
+import java.util.Random;
+
+public class PrimesThread implements Runnable {
+
+    private PrimesList primesList; //Creo un atributo de est clase para mantener una unic instancia en el main
+    
+    //Constructor
+    public PrimesThread(PrimesList primesList) {
+        this.primesList = primesList;
+    }
 
     @Override
     public void run() {
-        //Generar numero ramdon y determinar con el metodo isPrime si es primo para luego agregarlo a una lista
+        // Generar numero random y determinar con el metodo isPrime si es primo para luego agregarlo a una lista
+        try {
+            for (int i = 0; i < 10; i++) {
+                Thread.sleep(1000); // Pauso el hilo por 1 segundo
+                Random numeroRamdon = new Random(); //Comienzo con logica pra crear numero random
+                int min = 1;
+                int max = 100000;
+                int numeroGenerado = numeroRamdon.nextInt(max - min + 1) + min;
+                System.out.println(numeroGenerado);
+                //valido si el numero es primo, en caso de serlo, se agrega a la lista
+                if (primesList.isPrime(numeroGenerado)) {
+                    System.out.println("Es primo");
+                    primesList.add(numeroGenerado);
+                }
+            }
+            System.out.println("Tamaño de la lista: " + primesList.size()); //Imprimo el tamaño de lista para verificar concordancia con numeros guardados
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-    
 }
