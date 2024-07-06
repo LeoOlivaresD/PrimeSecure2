ffb1fc93922cad99959ed18ca0120a52ee28afe0
LeoOlivaresD <cspraword@gmail.com>
2024-07-06 17:30:55 -0400

    refactor

--- src/com/duoc/primesecure2/PrimeSecure2.java
+++ src/com/duoc/primesecure2/PrimeSecure2.java
@@ -1,6 +1,6 @@
 package com.duoc.primesecure2;
 
-import com.duoc.primesecure2.entities.Mensajeria;
+import com.duoc.primesecure2.entities.Mensaje;
 import com.duoc.primesecure2.entities.PrimesList;
 import com.duoc.primesecure2.input.ReaderPrimes;
 import com.duoc.primesecure2.output.MensajeEncriptado;
@@ -12,7 +12,7 @@ public class PrimeSecure2 {
 
     public static void main(String[] args) {
         // Creación de objetos
-        Mensajeria mensajeria = new Mensajeria();
+        Mensaje mensajeria = new Mensaje();
         PrimesList primesList = new PrimesList();
         PrimesThread runnable = new PrimesThread(primesList);//paso el objeto de primesList en el constructor de mi clase PrimesThread
         Thread thread = new Thread(runnable);
@@ -26,12 +26,14 @@ public class PrimeSecure2 {
 
         primesList.getPrimesCount();//finalmente devuelvo la lista de numeros primos
         System.out.println("Generando archivo csv");
-        WriterPrimesCsv.savePrimesListToCsv(primesList);
-        ReaderPrimes.readerCsv();
+        WriterPrimesCsv.savePrimesListToCsv(primesList); // guardo esa lista en un archivo csv
+        ReaderPrimes.readerCsv(); //Ademas implento metodo para poder leer el csv y añadir esos numeros a una lista
         
+        //Aca impleneto la logica para generar un mensaje
         System.out.println("Ingrese un mensaje");
         Scanner sc = new Scanner(System.in);
         String mensaje = sc.nextLine();
+        //Con este metodo guardo un mensaje, le asigno un id umero primo y ademas lo almaceno en un txt
         MensajeEncriptado.generarMensaje(mensajeria, primesList.get(0), mensaje);
         
         System.out.println(mensajeria.toString());
