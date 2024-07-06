40a16417ef3339098af539ff1ae7877acb15f97b
LeoOlivaresD <cspraword@gmail.com>
2024-07-06 18:53:54 -0400

    SE MODIFICA METODO ISPRIME PARA PASAR LA LISTA A UNA SINCRONIZADA

--- src/com/duoc/primesecure2/threads/PrimesThread.java
+++ src/com/duoc/primesecure2/threads/PrimesThread.java
@@ -1,17 +1,24 @@
 package com.duoc.primesecure2.threads;
 
 import com.duoc.primesecure2.entities.PrimesList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class PrimesThread implements Runnable {
 
     private PrimesList primesList; //Creo un atributo de est clase para mantener una unic instancia en el main
-    
+    private List<Integer> syncList;
     //Constructor
     public PrimesThread(PrimesList primesList) {
         this.primesList = primesList;
     }
 
+    public PrimesThread(PrimesList primesList, List<Integer> syncList) {
+        this.primesList = primesList;
+        this.syncList = syncList;
+    }
+    
     @Override
     public void run() {
         // Generar numero random y determinar con el metodo isPrime si es primo para luego agregarlo a una lista
@@ -27,6 +34,8 @@ public class PrimesThread implements Runnable {
                 if (primesList.isPrime(numeroGenerado)) {
                     System.out.println("Es primo");
                     primesList.add(numeroGenerado);
+                    //Luego de agregar ese numero a la lista lo paso a una lista sincronizada
+                    syncList = Collections.synchronizedList(primesList); //USO DE LISTA SINCRONIZADA
                 }
             }
             System.out.println("Tamaño de la lista: " + primesList.size()); //Imprimo el tamaño de lista para verificar concordancia con numeros guardados
