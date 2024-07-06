077fbac95f9c23d2bc325ac2fb1169d9bc939c8e
LeoOlivaresD <cspraword@gmail.com>
2024-07-06 17:27:56 -0400

    Se desarrollo metodo para crear archivo txt y almacenar mensajes encriptados por un id numero primo

--- src/com/duoc/primesecure2/output/MensajeEncriptado.java
+++ src/com/duoc/primesecure2/output/MensajeEncriptado.java
@@ -1,13 +1,25 @@
-
 package com.duoc.primesecure2.output;
 
-import com.duoc.primesecure2.entities.Mensajeria;
+import com.duoc.primesecure2.entities.Mensaje;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MensajeEncriptado {
-    
-     public static void generarMensaje(Mensajeria mensajeria, Integer idMensaje, String inputUsuario){
-        mensajeria.setMensaje(inputUsuario);
-        mensajeria.setIdMensaje(idMensaje);
-        mensajeria.getListaMensajes().add(mensajeria);
+
+    public static void generarMensaje(Mensaje mensaje, Integer idMensaje, String inputUsuario) {
+        mensaje.setMensaje(inputUsuario);
+        mensaje.setIdMensaje(idMensaje);
+        mensaje.getListaMensajes().add(mensaje);
+        //guardo el mensaje en un archivo de texto
+        guardarMensajeEnTxt(mensaje);
+    }
+
+    public static void guardarMensajeEnTxt(Mensaje mensaje) {
+        String salto = System.lineSeparator();
+        try (FileWriter fw = new FileWriter("listado mensajes.txt", true)) {
+            fw.write(mensaje.toString() + salto);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
