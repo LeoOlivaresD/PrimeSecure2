eab6a0a6ed7c8f120dcdf991b8ac80663f867a83
LeoOlivaresD <cspraword@gmail.com>
2024-07-07 20:59:27 -0400

    Se agrega metodo para desencriptar mensajes desde un txt

--- src/com/duoc/primesecure2/input/ReaderPrimes.java
+++ src/com/duoc/primesecure2/input/ReaderPrimes.java
@@ -1,5 +1,8 @@
 package com.duoc.primesecure2.input;
 
+import com.duoc.primesecure2.entities.Mensaje;
+import com.duoc.primesecure2.entities.PrimesList;
+import com.duoc.primesecure2.output.Encryptor;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -30,5 +33,31 @@ public class ReaderPrimes {
             e.printStackTrace();
         }
         return listaCsv;
+    }
+    //Con este metodo desencriptare mensajes desde un txt
+    public static List<Mensaje> leerMensajesEncriptados(String nombreArchivo, PrimesList primesList) {
+        List<Mensaje> mensajes = new ArrayList<>();
+        Encryptor encryptor = new Encryptor(primesList);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Parsear el mensaje y el id del archivo
+                String[] parts = line.split(", mensaje= ");
+                if (parts.length == 2) {
+                    String idPart = parts[0].replace("Mensaje{idMensaje = ", "").trim();
+                    String mensajePart = parts[1].replace('}', ' ').trim();
+
+                    Integer idMensaje = Integer.parseInt(idPart);
+                    String mensajeDesencriptado = encryptor.decryptMessage(mensajePart);
+
+                    Mensaje mensaje = new Mensaje(mensajeDesencriptado, idMensaje);
+                    mensajes.add(mensaje);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return mensajes;
     }
 }
