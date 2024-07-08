354237951d262f363ea3abdd82d8797a087a5f08
LeoOlivaresD <cspraword@gmail.com>
2024-07-07 20:54:10 -0400

    Se modifica metodo para poder encriptar el mensaje a numeros primos

--- src/com/duoc/primesecure2/output/MensajeEncriptado.java
+++ src/com/duoc/primesecure2/output/MensajeEncriptado.java
@@ -1,12 +1,22 @@
 package com.duoc.primesecure2.output;
 
 import com.duoc.primesecure2.entities.Mensaje;
+import com.duoc.primesecure2.entities.PrimesList;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class MensajeEncriptado {
+    private static PrimesList primesList;
+    private static Encryptor encryptor;
+    
+    public MensajeEncriptado(PrimesList primesList) {
+        MensajeEncriptado.primesList = primesList;
+        MensajeEncriptado.encryptor = new Encryptor(primesList);
+    }
+    
     public synchronized static void generarMensaje(Mensaje mensaje, Integer idMensaje, String inputUsuario) {
-        mensaje.setMensaje(inputUsuario);
+        String encryptedMessage = encryptor.encryptMessage(inputUsuario);
+        mensaje.setMensaje(encryptedMessage);
         mensaje.setIdMensaje(idMensaje);
         mensaje.getListaMensajes().add(mensaje);
         //guardo el mensaje en un archivo de texto
