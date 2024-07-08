f353196d0ad73ea49c05e730daafc99a94701e3d
LeoOlivaresD <cspraword@gmail.com>
2024-07-07 21:01:24 -0400

    Refactor

--- src/com/duoc/primesecure2/output/MensajeEncriptado.java
+++ src/com/duoc/primesecure2/output/MensajeEncriptado.java
@@ -1,6 +1,6 @@
 package com.duoc.primesecure2.output;
 
-import com.duoc.primesecure2.entities.Mensaje;
+import com.duoc.primesecure2.entities.Message;
 import com.duoc.primesecure2.entities.PrimesList;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,7 +14,7 @@ public class MensajeEncriptado {
         MensajeEncriptado.encryptor = new Encryptor(primesList);
     }
     
-    public synchronized static void generarMensaje(Mensaje mensaje, Integer idMensaje, String inputUsuario) {
+    public synchronized static void generarMensaje(Message mensaje, Integer idMensaje, String inputUsuario) {
         String encryptedMessage = encryptor.encryptMessage(inputUsuario);
         mensaje.setMensaje(encryptedMessage);
         mensaje.setIdMensaje(idMensaje);
@@ -23,7 +23,7 @@ public class MensajeEncriptado {
         guardarMensajeEnTxt(mensaje);
     }
 
-    public synchronized static void guardarMensajeEnTxt(Mensaje mensaje) {
+    public synchronized static void guardarMensajeEnTxt(Message mensaje) {
         String salto = System.lineSeparator();
         try (FileWriter fw = new FileWriter("listado mensajes.txt", true)) {
             fw.write(mensaje.toString() + salto);
